fe4904390ab3082c3db005f7659a89fa5f38d405
Luis Adrian Gonzalez Benavides <adrian.and1@gmail.com>
2012-02-04 18:28:05 -0430

    vistas

--- dataBase/ActualizarVistas.java
+++ dataBase/ActualizarVistas.java
@@ -6,7 +6,7 @@ import org.hibernate.classic.Session;
 
 /**
  * Crea las vistas en la base de datos
- * 
+ * @author Luis Adrian Gonzalez Benavides 
  * @author bc
  */
 public class ActualizarVistas {
@@ -15,68 +15,154 @@ public class ActualizarVistas {
      * Crea las vistas en la base de datos
      */
     public ActualizarVistas() {
+
         Session s = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = s.beginTransaction();
-        System.out.println(s.createSQLQuery("DROP TABLE IF EXISTS vista1,view_sumadetalle, view_agotamiento").executeUpdate());
-        String v = "";
-        v = "CREATE OR REPLACE VIEW view_agotamiento AS "
-                + "SELECT siniestro2_.asegurado_id, diagnostic0_.diagnostico_id, sum("
-                + "        CASE"
-                + "            WHEN upper(estatussin7_.nombre) = 'PAGADO' THEN diagnostic0_.montopagado"
-                + "            ELSE 0"
-                + "        END) AS montopagado, sum("
-                + "        CASE"
-                + "            WHEN upper(estatussin7_.nombre) = 'PENDIENTE' THEN diagnostic0_.montopendiente + diagnostic0_.montopagado"
-                + "            ELSE 0"
-                + "        END) AS montopendiente, siniestro2_.ayo"
-                + "   FROM sini_diagnosticosiniestro diagnostic0_, sini_detallesiniestro detallesin1_, sini_siniestro siniestro2_, sini_etapasiniestro etapasinie6_, sini_estatussiniestro estatussin7_"
-                + "  WHERE diagnostic0_.detallesiniestro_id = detallesin1_.id AND detallesin1_.siniestro_id = siniestro2_.id AND detallesin1_.etapasiniestro_id = etapasinie6_.id AND etapasinie6_.estatussiniestro_id = estatussin7_.id  GROUP BY siniestro2_.asegurado_id, siniestro2_.ayo, diagnostic0_.diagnostico_id;"
-                + "ALTER TABLE view_agotamiento OWNER TO postgres;";
-        System.out.println(s.createSQLQuery(v).executeUpdate());
-        v = "CREATE OR REPLACE VIEW view_sumadetalle AS "
-                + "SELECT sini_factura.detallesiniestro_id AS id,"
-                + " count(*) AS cantidadfacturas,"
+        System.out.println("DROP TABLES");
+        try {
+            System.out.println("view_sumafactura:");
+            s.createSQLQuery("DROP TABLE IF EXISTS view_sumafactura").executeUpdate();
+        } catch (Exception ex) {
+            tx.rollback();
+            tx = s.beginTransaction();
+        }
+        try {
+            System.out.println("view_sumadetalle:");
+            s.createSQLQuery("DROP TABLE IF EXISTS view_sumadetalle").executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            tx.rollback();
+        }
+        tx = s.beginTransaction();
+        try {
+            System.out.println("view_sumaorden:");
+            s.createSQLQuery("DROP TABLE IF EXISTS view_sumaorden").executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            tx.rollback();
+        }
+        tx = s.beginTransaction();
+        try {
+            System.out.println("view_sumaremesa:");
+            s.createSQLQuery("DROP TABLE IF EXISTS view_sumaremesa").executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            tx.rollback();
+        }
+        tx = s.beginTransaction();
+        try {
+            System.out.println("view_agotamiento:");
+            s.createSQLQuery("DROP TABLE IF EXISTS view_agotamiento").executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            tx.rollback();
+        }
+        tx = s.beginTransaction();
+
+        s.createSQLQuery(" CREATE OR REPLACE VIEW view_agotamiento AS "
+                + " SELECT siniestro2_.asegurado_id, diagnostic0_.diagnostico_id, sum("
+                + " CASE"
+                + " WHEN upper(estatussin7_.nombre) = 'PAGADO' THEN diagnostic0_.montopagado"
+                + " ELSE 0"
+                + " END) AS montopagado, sum("
+                + " CASE"
+                + " WHEN upper(estatussin7_.nombre) = 'PENDIENTE' THEN diagnostic0_.montopendiente + diagnostic0_.montopagado"
+                + " ELSE 0"
+                + " END) AS montopendiente, siniestro2_.ayo"
+                + " FROM sini_diagnosticosiniestro diagnostic0_, sini_detallesiniestro detallesin1_, sini_siniestro siniestro2_, sini_etapasiniestro etapasinie6_, sini_estatussiniestro estatussin7_"
+                + " WHERE diagnostic0_.detallesiniestro_id = detallesin1_.id AND detallesin1_.siniestro_id = siniestro2_.id AND detallesin1_.etapasiniestro_id = etapasinie6_.id AND etapasinie6_.estatussiniestro_id = estatussin7_.id GROUP BY siniestro2_.asegurado_id, siniestro2_.ayo, diagnostic0_.diagnostico_id;"
+                + " ALTER TABLE view_agotamiento OWNER TO postgres;"
+                + " DROP VIEW IF EXISTS view_sumafactura CASCADE;"
+                + " CREATE OR REPLACE VIEW view_sumafactura AS "
+                + " SELECT sini_factura.id, sum(sini_factura.totalliquidado * sini_factura.porcentajeretencionprontopago) AS montoretencionprontopago, sum(sini_factura.montoretenciondeducible + sini_factura.montoretencioniva + sini_factura.montoretencionisrl + sini_factura.montotm + sini_factura.totalliquidado * sini_factura.porcentajeretencionprontopago) AS totalretenido, sum(sini_factura.totalliquidado - (sini_factura.montoretenciondeducible + sini_factura.montoretencioniva + sini_factura.montoretenciontm + sini_factura.montoretencionisrl + sini_factura.totalliquidado * sini_factura.porcentajeretencionprontopago)) AS totalacancelar"
+                + " FROM sini_factura"
+                + " GROUP BY sini_factura.id;"
+                + " ALTER TABLE view_sumafactura OWNER TO postgres;"
+                + " CREATE OR REPLACE VIEW view_sumadetalle AS "
+                + " SELECT sini_factura.detallesiniestro_id AS id,"
+                + " count(sini_factura.id) AS cantidadfacturas,"
                 + " sum(sini_factura.baseislr) AS baseislr,"
-                + " sum(sini_factura.baseiva) AS baseiva, sum(sini_factura.gastosclinicos) AS gastosclinicos, "
-                + " sum(sini_factura.honorariosmedicos) AS honorariosmedicos, sum(sini_factura.montoamparado) AS montoamparado,"
-                + " sum(sini_factura.montodeducible) AS montodeducible, "
-                + " sum(sini_factura.montodescuentoprontopago) AS montodescuentoprontopago, sum(sini_factura.montoiva) AS montoiva,"
-                + " sum(sini_factura.montonoamparado) AS montonoamparado, sum(sini_factura.montoretencionisrl) AS montoretencionisrl, sum(sini_factura.montoretencioniva) AS montoretencioniva, sum(sini_factura.montotm) AS montotm, sum(sini_factura.sustraendo) AS sustraendo, sum(sini_factura.totalacancelar) AS totalacancelar, sum(sini_factura.totalfacturado) AS totalfacturado, sum(sini_factura.totalliquidado) AS totalliquidado, sum(sini_factura.totalretenido) AS totalretenido "
-                + "FROM sini_factura"
-                + "GROUP BY sini_factura.detallesiniestro_id;"
-                + "ALTER TABLE view_sumadetalle OWNER TO postgres;";
-        System.out.println(s.createSQLQuery(v).executeUpdate());
-        v = "CREATE OR REPLACE VIEW view_sumaorden AS "
-                + "SELECT detallesin1_.ordendepago_id AS id, count(sumadetall0_.id) AS cantidaddetalles, sum("
-                + "        CASE"
-                + "            WHEN titular5_.persona_id = asegurado8_.persona_id THEN 1"
-                + "            ELSE 0"
-                + "        END) AS numerosiniestrostitular, sum("
-                + "        CASE"
-                + "            WHEN titular5_.persona_id <> asegurado8_.persona_id THEN 1"
-                + "            ELSE 0"
-                + "        END) AS numerosiniestrosfamiliar, sum("
-                + "        CASE"
-                + "            WHEN titular5_.persona_id = asegurado8_.persona_id THEN sumadetall0_.montoamparado"
-                + "            ELSE 0::double precision"
-                + "        END) AS montotitulares, sum("
-                + "        CASE"
-                + "            WHEN titular5_.persona_id <> asegurado8_.persona_id THEN sumadetall0_.montoamparado"
-                + "            ELSE 0::double precision"
-                + "        END) AS montofamiliares, sum(sumadetall0_.baseislr) AS baseislr, sum(sumadetall0_.baseiva) AS baseiva, sum(sumadetall0_.cantidadfacturas) AS cantidadfactura, sum(sumadetall0_.gastosclinicos) AS gastosclinicos, sum(sumadetall0_.honorariosmedicos) AS honorariosmedicos, sum(sumadetall0_.montoamparado) AS montoamparado, sum(sumadetall0_.montodeducible) AS montodeducible, sum(sumadetall0_.montodescuentoprontopago) AS montodescuentoprontopago, sum(sumadetall0_.montoiva) AS montoiva, sum(sumadetall0_.montonoamparado) AS montonoamparado, sum(sumadetall0_.montoretencionisrl) AS montoretencionisrl, sum(sumadetall0_.montoretencioniva) AS montoretencioniva, sum(sumadetall0_.montotm) AS montotm, sum(sumadetall0_.sustraendo) AS sustraendo, sum(sumadetall0_.totalacancelar) AS totalacancelar, sum(sumadetall0_.totalfacturado) AS totalfacturado, sum(sumadetall0_.totalliquidado) AS totalliquidado, sum(sumadetall0_.totalretenido) AS totalretenido"
-                + "   FROM view_sumadetalle sumadetall0_"
-                + "  CROSS JOIN sini_detallesiniestro detallesin1_"
-                + "  CROSS JOIN sini_siniestro siniestro3_"
-                + "  CROSS JOIN aseg_certificado certificad4_     "
-                + "     CROSS JOIN aseg_titular titular5_"
-                + "  CROSS JOIN aseg_asegurado asegurado8_"
-                + "  WHERE sumadetall0_.id = detallesin1_.id AND detallesin1_.siniestro_id = siniestro3_.id AND siniestro3_.certificado_id = certificad4_.id AND certificad4_.titular_id = titular5_.id AND siniestro3_.asegurado_id = asegurado8_.id AND detallesin1_.ordendepago_id IS NOT NULL"
-                + "  GROUP BY detallesin1_.ordendepago_id;"
-                + "ALTER TABLE view_sumaorden OWNER TO postgres;";
+                + " sum(sini_factura.baseiva) AS baseiva,"
+                + " sum(sini_factura.gastosclinicos) AS gastosclinicos,"
+                + " sum(sini_factura.honorariosmedicos) AS honorariosmedicos,"
+                + " sum(sini_factura.montoamparado) AS montoamparado,"
+                + " sum(sini_factura.montoretenciondeducible) AS montoretenciondeducible,"
+                + " sum(view_sumafactura.montoretencionprontopago) AS montoretencionprontopago,"
+                + " sum(sini_factura.montoiva) AS montoiva,"
+                + " sum(sini_factura.montonoamparado) AS montonoamparado,"
+                + " sum(sini_factura.montoretencionisrl) AS montoretencionisrl,"
+                + " sum(sini_factura.montoretencioniva) AS montoretencioniva,"
+                + " sum(sini_factura.montoretenciontm) AS montoretenciontm,"
+                + " sum(view_sumafactura.totalacancelar) AS totalacancelar,"
+                + " sum(sini_factura.totalfacturado) AS totalfacturado,"
+                + " sum(sini_factura.totalliquidado) AS totalliquidado,"
+                + " sum(view_sumafactura.totalretenido) AS totalretenido"
+                + " FROM sini_factura"
+                + " CROSS JOIN view_sumafactura"
+                + " WHERE sini_factura.id = view_sumafactura.id"
+                + " GROUP BY sini_factura.detallesiniestro_id;"
+                + " ALTER TABLE view_sumadetalle OWNER TO postgres;"
+                + " CREATE OR REPLACE VIEW view_sumaorden AS "
+                + " SELECT detallesin1_.ordendepago_id AS id, count(sumadetall0_.id) AS cantidaddetalles, sum("
+                + " CASE"
+                + " WHEN titular5_.persona_id = asegurado8_.persona_id THEN 1"
+                + " ELSE 0"
+                + " END) AS numerosiniestrostitular, sum("
+                + " CASE"
+                + " WHEN titular5_.persona_id <> asegurado8_.persona_id THEN 1"
+                + " ELSE 0"
+                + " END) AS numerosiniestrosfamiliar, sum("
+                + " CASE"
+                + " WHEN titular5_.persona_id = asegurado8_.persona_id THEN sumadetall0_.totalfacturado"
+                + " ELSE 0"
+                + " END) AS montotitulares, sum("
+                + " CASE"
+                + " WHEN titular5_.persona_id <> asegurado8_.persona_id THEN sumadetall0_.totalfacturado"
+                + " ELSE 0"
+                + " END) AS montofamiliares, sum(sumadetall0_.baseislr) AS baseislr, sum(sumadetall0_.baseiva) AS baseiva, sum(sumadetall0_.cantidadfacturas) AS cantidadfacturas, sum(sumadetall0_.gastosclinicos) AS gastosclinicos, sum(sumadetall0_.honorariosmedicos) AS honorariosmedicos, sum(sumadetall0_.montoamparado) AS montoamparado, sum(sumadetall0_.montoretenciondeducible) AS montoretenciondeducible, sum(sumadetall0_.montoretencionprontopago) AS montoretencionprontopago, sum(sumadetall0_.montoiva) AS montoiva, sum(sumadetall0_.montonoamparado) AS montonoamparado, sum(sumadetall0_.montoretencionisrl) AS montoretencionisrl, sum(sumadetall0_.montoretencioniva) AS montoretencioniva, sum(sumadetall0_.montoretenciontm) AS montoretenciontm, sum(sumadetall0_.totalacancelar) AS totalacancelar, sum(sumadetall0_.totalfacturado) AS totalfacturado, sum(sumadetall0_.totalliquidado) AS totalliquidado, sum(sumadetall0_.totalretenido) AS totalretenido"
+                + " FROM view_sumadetalle sumadetall0_"
+                + " CROSS JOIN sini_detallesiniestro detallesin1_"
+                + " CROSS JOIN sini_siniestro siniestro3_"
+                + " CROSS JOIN aseg_certificado certificad4_"
+                + " CROSS JOIN aseg_titular titular5_"
+                + " CROSS JOIN aseg_asegurado asegurado8_"
+                + " WHERE sumadetall0_.id = detallesin1_.id AND detallesin1_.siniestro_id = siniestro3_.id AND siniestro3_.certificado_id = certificad4_.id AND certificad4_.titular_id = titular5_.id AND siniestro3_.asegurado_id = asegurado8_.id AND detallesin1_.ordendepago_id IS NOT NULL"
+                + " GROUP BY detallesin1_.ordendepago_id;"
+                + " ALTER TABLE view_sumaorden OWNER TO postgres;"
+                + " CREATE OR REPLACE VIEW view_sumaremesa AS "
+                + " SELECT pago_ordendepago.remesa_id AS id,"
+                + " count(view_sumaorden.id) AS cantidadordenes,"
+                + " sum(view_sumaorden.cantidaddetalles) AS cantidaddetalles,"
+                + " sum(view_sumaorden.numerosiniestrostitular) AS numerosiniestrostitular,"
+                + " sum(view_sumaorden.numerosiniestrosfamiliar) AS numerosiniestrosfamiliar,"
+                + " sum(view_sumaorden.montotitulares) AS montotitulares,"
+                + " sum(view_sumaorden.montofamiliares) AS montofamiliares,"
+                + " sum(view_sumaorden.baseislr) AS baseislr,"
+                + " sum(view_sumaorden.baseiva) AS baseiva,"
+                + " sum(view_sumaorden.cantidadfacturas) AS cantidadfacturas,"
+                + " sum(view_sumaorden.gastosclinicos) AS gastosclinicos,"
+                + " sum(view_sumaorden.honorariosmedicos) AS honorariosmedicos,"
+                + " sum(view_sumaorden.montoamparado) AS montoamparado,"
+                + " sum(view_sumaorden.montoretenciondeducible) AS montoretenciondeducible,"
+                + " sum(view_sumaorden.montoretencionprontopago) AS montoretencionprontopago,"
+                + " sum(view_sumaorden.montoiva) AS montoiva,"
+                + " sum(view_sumaorden.montonoamparado) AS montonoamparado,"
+                + " sum(view_sumaorden.montoretencionisrl) AS montoretencionisrl,"
+                + " sum(view_sumaorden.montoretencioniva) AS montoretencioniva,"
+                + " sum(view_sumaorden.montoretenciontm) AS montoretenciontm,"
+                + " sum(view_sumaorden.totalacancelar) AS totalacancelar,"
+                + " sum(view_sumaorden.totalfacturado) AS totalfacturado,"
+                + " sum(view_sumaorden.totalliquidado) AS totalliquidado,"
+                + " sum(view_sumaorden.totalretenido) AS totalretenido"
+                + " FROM view_sumaorden"
+                + " CROSS JOIN pago_ordendepago"
+                + " WHERE view_sumaorden.id = pago_ordendepago.id"
+                + " GROUP BY pago_ordendepago.remesa_id;"
+                + " ALTER TABLE view_sumaremesa OWNER TO postgres;").executeUpdate();
+
         tx.commit();
-        System.out.println("comit");
+        System.out.println("Vistas Creadas");
         s.close();
-        System.out.println("close");
         System.out.println("Listo");
     }
 
